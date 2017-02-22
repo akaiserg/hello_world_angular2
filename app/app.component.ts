a7df3e5e367094a7c1321da87fb36ed0a16237b5
Andres Kaiser <akais002@gmail.com>
2017-02-22 00:11:24 -0300

    first commit

---
+++ app/app.component.ts
@@ -0,0 +1,13 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>Hello {{title}}</h1><span>{{count}}<span><button (click)="increaseCount()">Click!</button>'
+})
+export class AppComponent {
+    count = 0;
+    tittle = "Hello Word";
+    increaseCount(){
+        this.count++;
+    }
+ }
